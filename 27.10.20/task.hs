b685f780e0dd5bd54a3c4f318bb2f61c8c5d0cbf
criempie <criempie@localhost.localdomain>
2020-11-10 13:29:31 +0500

    rework 2 4 5 6

--- 27.10.20/task.hs
+++ 27.10.20/task.hs
@@ -3,16 +3,19 @@ fact :: Int -> Int
 fact n = if n <= 0 then 1 else n * fact (n-1)
 
 -- task2
+
 gcd' :: Int -> Int -> Int
-gcd' a b = if (mod (max a b) (min a b)) == 0 
-        then (min a b)
-        else gcd' (min a b) (mod (max a b) (min a b))
+gcd' a b = if b == 0 
+        then a
+        else gcd' b (mod a b)
 
 -- task3
 isPrime :: Int -> Bool
 isPrime a = if a == 2 || a == 3
         then True
-        else isPrime_ a 2
+        else if a < 2 
+                then False
+                else isPrime_ a 2
 
 isPrime_ :: Int -> Int -> Bool
 isPrime_ a n = if n^2 <= a
@@ -23,30 +26,25 @@ isPrime_ a n = if n^2 <= a
 
 -- task4
 reverseNumber :: Integer -> Integer
-reverseNumber a = reverseNumber_ a (lengthNumber a)
+reverseNumber a = reverseNumber_ a 0
 
 reverseNumber_ :: Integer -> Integer -> Integer
-reverseNumber_ a n = if n == 1 
-        then a
-        else (mod a 10) * 10 ^ (n - 1) + reverseNumber_ (div a 10) (n - 1)
-
-lengthNumber :: Integer -> Integer
-lengthNumber a = if a == 0
-        then 0
-        else 1 + lengthNumber (div a 10)
+reverseNumber_ a b = if a == 0
+        then b 
+        else reverseNumber_ (a `div` 10) (b * 10 + a `mod` 10)
 
 --  task5 
 maxRoot :: Double -> Double -> Double -> Double
 maxRoot a b c = if (b ^ 2 - 4 * a * c) < 0 
         then 0/0
-        else (max ((-b - (sqrt (b ^ 2 - (4 * a * c))))/(2 * a)) ((-b + (sqrt (b ^ 2 - (4 * a * c))))/(2 * a)))
+        else max ((-b - sqrt (b^2 - 4*a*c))/(2*a)) 
+                ((-b + sqrt (b^2 - 4*a*c))/(2*a))
 
 -- task6
 root :: (Double->Double) -> Double -> Double -> Double -> Double
-root f a b eps = if (f (a + b)/2) <= eps
+root f a b eps = if b - a <= eps
         then (a + b)/2
         else  if (f (a + b)/2) * f a <= 0
                 then root f a ((a + b)/2) eps
-                else if (f (a + b)/2) * f b <= 0 
-                        then root f ((a + b)/2) b eps
-                        else 0/0
+                else root f ((a + b)/2) b eps
+                        
